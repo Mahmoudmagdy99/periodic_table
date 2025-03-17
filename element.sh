9b47ca139700a1b657eaf8eb95ba1dac2e1d0d89
Mahmoudmagdy99 <mahmoudkotamy97@gmail.com>
2025-03-16 22:44:38 -0400

    feat: add Determine search condition

--- element.sh
+++ element.sh
@@ -8,3 +8,13 @@ then
   echo "Please provide an element as an argument."
   exit
 fi
+# Determine search condition
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  CONDITION="atomic_number=$1"
+elif [[ $1 =~ ^[A-Za-z]{1,2}$ ]]
+then
+  CONDITION="symbol='$1'"
+else
+  CONDITION="name='$1'"
+fi
